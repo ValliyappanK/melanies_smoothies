0f359f39a8e81f0bdd9de9641f6fdd76a5fc47e4
Valliyappan Kannappan <105097552+ValliyappanK@users.noreply.github.com>
2025-05-05 12:47:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import snowflake.connector
+from urllib.error import URLError
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie  :cup_with_straw:")
@@ -11,12 +13,7 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:" +
 name_on_order)
 
-cnx = st.connection("",
-                    type="snowflake",
-                    account="TWHPEYI-ILB63558",
-                    user="SNOWTRIAL01",
-                    authenticator="externalbrowser"
-                   )
+cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
